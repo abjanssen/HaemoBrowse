1e471c3bc89f9b2b6b4a5ecae2c0b11ce8badc53
Axel Janssen <janssen.axel@gmail.com>
2024-06-28 15:10:02 +0200

    v0.2

---
+++ plugins/RemoveCOG.js
@@ -0,0 +1,17 @@
+// RemoveCOG.js
+;(function () {
+  class MyPlugin {
+    install() {}
+    configure(pluginManager) {
+      pluginManager.jexl.addFunction('removecog', feature => {
+        return feature.replace(/COG:/g, ""); 
+      })
+    }
+  }
+
+  // the plugin will be included in both the main thread and web worker, so
+  // install plugin to either window or self (webworker global scope)
+  ;(typeof self !== 'undefined' ? self : window).JBrowsePluginRemoveCOG = {
+    default: MyPlugin,
+  }
+})()
